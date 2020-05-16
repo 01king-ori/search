8c69e31e882a606a4eae6966cc6c1d761fdc1c60
Vincent <chridtinewanja@gmail.com>
2020-05-16 21:14:06 +0300

    this adds a pipe for the project

--- app.module.ts
+++ app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { UserComponentComponent } from './components/user-component/user-component.component';
+import { ErrorComponent } from './components/error/error.component';
+import { ResultsComponent } from './components/results/results.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { PipePipe } from './pipe.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    UserComponentComponent,
+    ErrorComponent,
+    ResultsComponent,
+    NavBarComponent,
+    PipePipe
   ],
   imports: [
     BrowserModule,
